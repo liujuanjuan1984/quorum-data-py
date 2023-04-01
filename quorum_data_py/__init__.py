93c3037867082358f6bb4a26dd774f9eae3c8155
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2023-04-02 00:56:18 +0800

    [1.1.1]add get-trx-type

--- quorum_data_py/__init__.py
+++ quorum_data_py/__init__.py
@@ -1,10 +1,13 @@
 import logging
 
 from quorum_data_py import converter, feed
+from quorum_data_py.trx_type import get_trx_type
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __author__ = "liujuanjuan1984"
 
 # Set default logging handler to avoid "No handler found" warnings.
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
+
+logger.info("quorum_data_py version: %s", __version__)
---
+++ quorum_data_py/trx_type.py
@@ -0,0 +1,37 @@
+def get_trx_type(trx: dict):
+    """
+    获取 trx 的类型，和 rum-feed js 的定义保持一致。
+    https://github.com/okdaodine/rum-feed/blob/main/server/utils/getTrxType.js
+    返回结果：
+    unknown/post/comment/counter/profile/delete/relation/wallet
+    """
+    data = trx.get("Data") or trx.get("Content")  # 兼容新旧链
+    if not data or isinstance(data, str):
+        raise ValueError("trx is encrypted.")
+
+    out_type = data.get("type")
+    inner_type = data.get("object", {}).get("type")
+    trx_type = "unknown"
+    if out_type == "Create":
+        if inner_type == "Note":
+            inreplyto = data.get("object", {}).get("inreplyto")
+            if not inreplyto:
+                trx_type = "post"
+            elif inreplyto.get("type") == "Note":
+                trx_type = "comment"
+        elif inner_type == "Profile":
+            trx_type = "profile"
+    elif out_type == "Like" or (out_type == "Undo" and inner_type == "Like"):
+        trx_type = "counter"
+    elif out_type == "Delete" and inner_type == "Note":
+        trx_type = "delete"
+    elif (
+        out_type == "Follow"
+        or (out_type == "Undo" and inner_type == "Follow")
+        or out_type == "Block"
+        or (out_type == "Undo" and inner_type == "Block")
+    ):
+        trx_type = "relation"
+    elif out_type == "Announce" and data.get("name").find("private key") != -1:
+        trx_type = "wallet"
+    return trx_type
